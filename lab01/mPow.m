afe2d2fdd0ca7aac58d5127a1b60b80240804ab4
lespam <lesliepbv@gmail.com>
2018-01-30 14:34:21 -0600

    Intento 2

--- lab01/mPow.m
+++ lab01/mPow.m
@@ -1,13 +1,19 @@
-function [ sigma, w ] = mPow( q, A, k )
+function [ sigma, w ] = mPow( q, A, TOL )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    for i = 1:k
+
+    %Tolerancia
+    sigma = 25;
+    w = 0;
+    tolerancia=1;
+    error = -1;
+    while tolerancia<=error
         w = q/norm(q);
         q = A*w;
         sigma = cross(w,A*w);
+        tolerancia = norm(A*w - sigma*w);
+        error = TOL*norm(A*w);
     end
     w = q/norm(q);
-    sigma
-    w
 end
 
