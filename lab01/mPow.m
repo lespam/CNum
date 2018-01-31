f46ea673dea35593027294699d9a81aa477169b7
Les Brenes <lesliepbv@gmail.com>
2018-01-30 21:06:42 -0600

    Lo logré

--- lab01/mPow.m
+++ lab01/mPow.m
@@ -1,19 +1,17 @@
-function [ sigma, w ] = mPow( q, A, TOL )
+
+function [ error, sigma, w ] = mPow( q, A, TOL )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
     %Tolerancia
     sigma = 25;
-    w = 0;
-    tolerancia=1;
-    error = -1;
-    while tolerancia<=error
+    error = 1;
+    
+    while error>TOL
         w = q/norm(q);
         q = A*w;
-        sigma = cross(w,A*w);
-        tolerancia = norm(A*w - sigma*w);
-        error = TOL*norm(A*w);
+        sigma = dot(w,q);
+        error = norm(A*w - sigma*w)/norm(A*w)
     end
     w = q/norm(q);
 end
-
