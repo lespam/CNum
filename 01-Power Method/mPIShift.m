2d19f3b59cc8f8c1571dd1ad812c9e450f23f8ef
lespam <lesliepbv@gmail.com>
2018-02-27 14:28:04 -0600

    prac1

--- 01-Power Method/mPIShift.m
+++ 01-Power Method/mPIShift.m
@@ -1,7 +1,7 @@
 % Author: LesPam
-function [lam1, w] = mPI( A, q0, k, TOL )
+function [lamn, w] = mPIinvShift(A, q0, rho, k, tol)
 % Power Method 
-% This algorithm implements the Power Method to a Matrix
+% This algorithm implements the Power Method with shift to a Matrix
 % In  : q       initial vector
 %       A       square Matrix
 %       TOL     tolerance error number
@@ -12,11 +12,13 @@ function [lam1, w] = mPI( A, q0, k, TOL )
     %Tolerancia
     error = 1;
     it = 0;
-    while error>TOL && k>=it
+    A = A-(rho*eye(size(A)));
+    
+    while error>tol && k>=it
         w = q0/norm(q0);
-        q0 = A*w;
-        lam1 = dot(w,q0);
-        error = norm(A*w - lam1*w)/norm(A*w);
+        q0 = A\w;
+        lamn = dot(w,q0);
+        error = norm(A\w - lamn*w)/norm(A\w);
         it = it+1;
     end
     w = q0/norm(q0);
--- 01-Power Method/mPIinvRayleigh.m
+++ 01-Power Method/mPIinvRayleigh.m
@@ -1,7 +1,7 @@
 % Author: LesPam
-function [lam1, w] = mPI( A, q0, k, TOL )
+function [lamj, w] = mPIinvRayleigh(A, q0, k)
 % Power Method 
-% This algorithm implements the Power Method to a Matrix
+% This algorithm implements the Power Method with shift to a Matrix
 % In  : q       initial vector
 %       A       square Matrix
 %       TOL     tolerance error number
@@ -10,13 +10,17 @@ function [lam1, w] = mPI( A, q0, k, TOL )
 %       w       dominant eigenvector
 
     %Tolerancia
+    TOL = 10^-7;
     error = 1;
     it = 0;
+    rho = (htranspose(q0)*A*q)/(htranspose(q0)*q0);
+    
+    A = A-(rho*eye(size(A)));
     while error>TOL && k>=it
         w = q0/norm(q0);
-        q0 = A*w;
-        lam1 = dot(w,q0);
-        error = norm(A*w - lam1*w)/norm(A*w);
+        q0 = A\w;
+        lamj = dot(w,q0);
+        error = norm(A*w - rho*w)/norm(w);
         it = it+1;
     end
     w = q0/norm(q0);
