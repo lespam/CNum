07b8cb4f17e7852e671a32f69ff237b31e374725
lespam <lesliepbv@gmail.com>
2018-03-11 21:21:45 -0600

    Proyecto

--- Practica_01/ejercicio_34.m
+++ Practica_01/ejercicio_34.m
@@ -1,26 +1,61 @@
-clear all;
-close all;
-clc;
-%ejercicio_34
-A = [1,1,2;-1,9,3;0,-1,3];
-q1_0 = [11,92,79]';
-q2_0 = [-61,-21,99]';
-q3_0 = [31,32,-68]';
-k = 10;
-
-
-[lam1, w1] = mPIinvRayleigh(A,q1_0,k)
-[lam2, w2] = mPIinvRayleigh(A,q2_0,k)
-[lam3, w3] = mPIinvRayleigh(A,q3_0,k)
-[V,U] = eig(A)
-v1 = -V(:,2);
-v2 = V(:,1);
-v3 = V(:,3);
-for i = 1:k
-  [l, z] = mPIinvRayleigh(A,q2_0,i);
-
-  error(i) = abs(l-U(2,2));
- 
-end
-error
-
+clear all;
+
+close all;
+
+clc;
+
+%ejercicio_34
+
+A = [1,1,2;-1,9,3;0,-1,3];
+
+q1_0 = [11,92,79]';
+
+q2_0 = [-61,-21,99]';
+
+q3_0 = [31,32,-68]';
+
+k = 10;
+
+
+
+
+
+[lam1, w1] = mPIinvRayleigh(A,q1_0,k)
+
+[lam2, w2] = mPIinvRayleigh(A,q2_0,k)
+
+[lam3, w3] = mPIinvRayleigh(A,q3_0,k)
+
+[V,U] = eig(A)
+
+v2 = -V(:,2);
+
+v1 = V(:,1);
+
+v3 = -V(:,3);
+valores_num = [lam1 lam2 lam3;U(1,1) U(2,2) U(3,3)]
+
+lambdas = array2table(valores_num,...
+    'VariableNames',{'lam1','lam2','lam3'},...
+    'RowNames',{'num?rico','real'})
+
+vectores_numerico = [w1,w2,w3];
+vectores_reales = [v1,v2,v3];
+
+vectores_num = array2table(vectores_numerico,...
+    'VariableNames',{'vec_Num1','vec_Num2','vec_Num3'})
+vectores_real = array2table(vectores_reales,...
+    'VariableNames',{'vec_Real1','vec_Real2','vec_Real3'})
+
+
+for i = 1:k
+
+  [l, z] = mPIinvRayleigh(A,q2_0,i);
+
+  error(i) = abs(l-U(2,2));
+end
+
+error
+
+
+
--- Practica_01/ejercicio_37.m
+++ Practica_01/ejercicio_37.m
@@ -1,7 +1,38 @@
-clear all;
-
-close all;
-
-clc;
-
+clear all;   close all;   clc;
 %ejercicio_37
+
+mySvd = @(A)  mSvdEco(A);
+
+
+RGB = imread('small.jpg');
+
+figure(1);
+imshow(RGB);
+
+
+
+figure(2);
+A = [RGB(:,:,1); RGB(:,:,2); RGB(:,:,3)];
+[m,n] = size(A);
+fprintf('max-rank %g\n', min(m,n));
+
+m = m/3;
+A = double(A); % for svd routine
+[U,S,V] = mySvd(A);
+
+rmax = numel(diag(S));
+fprintf('numerical-rank %g\n', rmax);
+%for r = 1
+for r = [1,10,ceil(0.1*rmax), ceil(0.25*rmax)]
+	fprintf('current-rank %g\n', r);
+	% reconstruct image
+	B = U(:,1:r)*S(1:r,1:r)*V(:,1:r)';
+
+	% convert image for display
+	B = uint8(B);
+	C = cat(3, B(1:m,:), B(m+1:2*m,:), B(2*m+1:3*m,:));
+	h = imshow(C);
+    
+	pause
+end
+
